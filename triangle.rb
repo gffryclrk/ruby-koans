1847ee88912787513705f770ff0a74c4ba161f6b
Geoffrey Clark <clark@justabeauty.com>
2014-11-13 09:57:28 -0500

    161/282 Complete

--- triangle.rb
+++ triangle.rb
@@ -15,7 +15,22 @@
 #
 def triangle(a, b, c)
   # WRITE THIS CODE
+  raise TriangleError, "Sides must be greater than 0" if (a <= 0 || b <= 0 || c <= 0)
+  raise TriangleError, "The sum of two sides must be greater than the third" if ( a + b <= c || a + c <= b || b + c <= a)
+  # I definitely used the hint link provided in about_triangle_project_2.rb. 
+
+  if ((a == b) and (b == c) && (a == c))
+  	:equilateral
+  elsif (( a == b and b != c ) || ( a == c && a != b) || ( b == c && b != a ))
+  	:isosceles
+  elsif ((a != b) and (b != c) and (a != c))
+  	:scalene
+  end  	
 end
+
+# Above passes the test but I found some much more clever ways to do this on StackOverflow.
+# I left my first attempt though because it feels honest. I guess refactoring would be a good idea though.
+# A lot of the above code is unnecessary. There's also some interesting ways to use .sort and .uniq to accomplish the same. 
 
 # Error class used in part 2.  No need to change this code.
 class TriangleError < StandardError
